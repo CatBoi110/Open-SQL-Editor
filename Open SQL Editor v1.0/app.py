0484215cdd80daf0b05fde517dcab7d82cc832fc
Jack Levin <jlevin110@outlook.com>
2023-05-31 08:09:01 -0700

    Add files via upload

---
+++ Open SQL Editor v1.0/app.py
@@ -0,0 +1,8 @@
+import eel
+
+from login.loginFunctions import *
+from table.tableFunctions import *
+
+eel.init("web", allowed_extensions=[".js", ".html"])
+
+eel.start("table.html", mode="default", port = 8080)
