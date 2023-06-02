c5b6ee7d259ef76d8d07f790bce4eddedc098efc
Jack Levin <jlevin110@outlook.com>
2023-06-02 11:34:09 -0400

    Delete v1.0 directory

--- v1.0/app.py
+++
@@ -1,8 +0,0 @@
-import eel
-
-from login.loginFunctions import *
-from table.tableFunctions import *
-
-eel.init("web", allowed_extensions=[".js", ".html"])
-
-eel.start("table.html", mode="default", port=8080)
